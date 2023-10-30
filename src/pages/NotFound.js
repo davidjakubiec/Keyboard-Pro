af56b9cecc04d7e21aee99297c9d8ac4906289bb
DavidJakubiec <dwjakubiec@gmail.com>
2023-10-30 15:57:32 -0700

    fixed routing issue with useNavigate hook

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,18 @@
-import React, { useEffect } from 'react'
-import { Link } from "react-router-dom"
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from "react-router-dom"
 
 
 
 const NotFound = () => {
+const [redirect, setRedirect] = useState(true);
+const navigate = useNavigate();
   useEffect(() => {
-  window.location.href = 'https://davidjakubiec.github.io/Keyboard-Pro/'
+    navigate('/')
+    // if (redirect) {
+    //   // window.location.href = 'https://davidjakubiec.github.io/Keyboard-Pro/'
+    //   window.location.href = '/'
+    //   setRedirect(false)
+    // }
 },[])
   return (
     <div>
