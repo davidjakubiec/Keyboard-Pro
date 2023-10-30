8b857784c1396c638aec24614b41b8239eb1c790
DavidJakubiec <dwjakubiec@gmail.com>
2023-10-30 15:36:22 -0700

    fixed Not Found on initial page load routing issue

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from "react-router-dom"
 
+
+
 const NotFound = () => {
+  useEffect(() => {
+  window.location.href = 'http://localhost:3010'
+},[])
   return (
     <div>
-        <h2>Page not found!</h2>
+        {/* <h2>Page not found!</h2>
         <p>You're doing great!</p>
 
-        <p>Go to the <Link to='/'>Homepage</Link></p>
+        <p>Go to the <Link to='/'>Homepage</Link></p> */}
     </div>
   )
 }
